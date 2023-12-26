f4f9d5653aaaddaed3fe903e80801baf176c3a4a
lksenya <145290315+lksenya@users.noreply.github.com>
2023-12-26 19:31:51 +0300

    Update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,46 +1,107 @@
 #include <iostream>
-#include <cctype>
+#include "modAlphaCipher.h"
+#include <UnitTest++/UnitTest++.h>
 #include <locale>
-#include "modalphacipher.h"
-#include <codecvt>
-#include <typeinfo>
+#include <string>
+#include <ostream>
 
-using namespace std;
-
-void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+SUITE(KeyTest)
 {
-    try {
-        wstring cipherText;
-        wstring decryptedText;
-        modalphacipher cipher(key);
-        cipherText = cipher.encrypt(Text); // зашифровывание
-        if (destructCipherText) // надо "портить"?
-            cipherText.front() = tolower(cipherText.front()); // "портим"
-        decryptedText = cipher.decrypt(cipherText); // расшифровывание
-        wcout<<L"key="<<key<<endl;
-        wcout<<Text<<endl;
-        wcout<<cipherText<<endl;
-        wcout<<decryptedText<<endl;
-    } catch (const cipher_error & e) {
-        wcerr<<"Error: "<<e.what()<<endl;
+    TEST(ValidKey) {
+        CHECK_EQUAL("BCDBC",modAlphaCipher("BCD").encrypt("AAAAA"));
+    }
+    TEST(LongKey) {
+        CHECK_EQUAL("BCDEF",modAlphaCipher("BCDEFGHIJK").encrypt("AAAAA"));
+    }
+    TEST(LowCaseKey) {
+        CHECK_THROW(modAlphaCipher("bcd"), cipher_error );
+    }
+    TEST(DigitsInKey) {
+        CHECK_THROW(modAlphaCipher cp("B1"),cipher_error);
+    }
+    TEST(PunctuationInKey) {
+        CHECK_THROW(modAlphaCipher cp("B,C"),cipher_error);
+    }
+    TEST(WhitespaceInKey) {
+        CHECK_THROW(modAlphaCipher cp("B C"),cipher_error);
+    }
+    TEST(EmptyKey) {
+        CHECK_THROW(modAlphaCipher cp(""),cipher_error);
+    }
+    TEST(WeakKey) {
+        CHECK_THROW(modAlphaCipher cp("AAA"),cipher_error);
     }
 }
 
-// проверка, чтобы строка состояла только из прописных букв
-bool isValid(const wstring& s)
+struct KeyB_fixture {
+    modAlphaCipher * p;
+    KeyB_fixture()
+    {
+        p = new modAlphaCipher("B");
+    }
+    ~KeyB_fixture()
+    {
+        delete p;
+    }
+};
+
+SUITE(EncryptTest)
 {
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
-    return true;
+    TEST_FIXTURE(KeyB_fixture, UpCaseString) {
+        CHECK_EQUAL("UIFRVJDLCSPXOGPYKVNQTPWFSUIFMBAZEPH",
+                    p->encrypt("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"));
+    }
+    TEST_FIXTURE(KeyB_fixture, LowCaseString) {
+        CHECK_EQUAL("UIFRVJDLCSPXOGPYKVNQTPWFSUIFMBAZEPH",
+                    p->encrypt("thequickbrownfoxjumpsoverthelazydog"));
+    }
+    TEST_FIXTURE(KeyB_fixture, StringWithWhitspaceAndPunct) {
+        CHECK_EQUAL("UIFRVJDLCSPXOGPYKVNQTPWFSUIFMBAZEPH",
+                    p->encrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG!!!"));
+    }
+    TEST_FIXTURE(KeyB_fixture, StringWithNumbers) {
+        CHECK_EQUAL("IBQQZOFXZFBS", p->encrypt("Happy New 2019 Year"));
+    }
+    TEST_FIXTURE(KeyB_fixture, EmptyString) {
+        CHECK_THROW(p->encrypt(""),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, NoAlphaString) {
+        CHECK_THROW(p->encrypt("1234+8765=9999"),cipher_error);
+    }
+    TEST(MaxShiftKey) {
+        CHECK_EQUAL("SGDPTHBJAQNVMENWITLORNUDQSGDKZYXCNF",
+                    modAlphaCipher("Z").encrypt("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"));
+    }
 }
-int main()
+
+SUITE(DecryptText)
 {
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    check(L"ПРИВЕТ",L"ПРОЩАЙ");
-    check(L"ПРОЩАЙ",L"");
-    check(L"ПРОЩАЙ",L"ПРИВЕТ321");
-    check(L"П Р О Щ А Й",L"ПРИВЕТ");
-    check(L"321",L"ПРИВЕТ");
+    TEST_FIXTURE(KeyB_fixture, UpCaseString) {
+        CHECK_EQUAL("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG",
+                    p->decrypt("UIFRVJDLCSPXOGPYKVNQTPWFSUIFMBAZEPH"));
+    }
+    TEST_FIXTURE(KeyB_fixture, LowCaseString) {
+        CHECK_THROW(p->decrypt("uifRVJDLCSPXOGPYKVNQTPWFSUIFMBAZEPH"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, WhitespaceString) {
+        CHECK_THROW(p->decrypt("UIF RVJDL CSPXO GPY KVNQT PWFS UIF MBAZ EPH"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, DigitsString) {
+        CHECK_THROW(p->decrypt("IBQQZOFX2019ZFBS"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, PunctString) {
+        CHECK_THROW(p->decrypt("IFMMP,XPSME"),cipher_error);
+    }
+    TEST_FIXTURE(KeyB_fixture, EmptyString) {
+        CHECK_THROW(p->decrypt(""),cipher_error);
+    }
+    TEST(MaxShiftKey) {
+        CHECK_EQUAL("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG",
+                    modAlphaCipher("Z").decrypt("SGDPTHBJAQNVMENWITLORNUDQSGDKZYXCNF"));
+    }
+}
+
+int main(int argc, char **argv)
+{
+    return UnitTest::RunAllTests();
 }
